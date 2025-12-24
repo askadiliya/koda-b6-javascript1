aaa4d7ec5d3d869697f11ccba3fd9c1e16b95e95
Aska Diliyan Perkasa <93531147+askadiliya@users.noreply.github.com>
2025-12-24 11:13:01 +0700

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,8 @@
+const phi = 3.14;
+const r = 17;
+
+const luasLingkaran = `luas lingkaran adalah ${phi * r * r}`;
+console.log(luasLingkaran);
+
+const kelilingLingkaran = `keliling lingkaran adalah ${2 * phi * r}`;
+console.log(kelilingLingkaran);
